0c47e8d57b1603c5a90fcf3b265596d7bab197da
marco-amorim <marcoamorim95@hotmail.com>
2020-11-12 21:23:31 -0300

    cleaning components with afterEach

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -10,6 +10,10 @@ beforeEach(() => {
 	wrapped = shallow(<CommentBox />);
 });
 
+afterEach(() => {
+	wrapped.unmount();
+});
+
 it('has a text area and a button', () => {
 	expect(wrapped.find('textarea').length).toEqual(1);
 	expect(wrapped.find('button').length).toEqual(1);
