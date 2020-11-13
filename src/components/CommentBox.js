7a7da1df48e9a99dddc6f59d2e5a58f22d3953a7
marco-amorim <marcoamorim95@hotmail.com>
2020-11-12 21:20:59 -0300

    testing commentbox component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const CommentBox = () => {
-	return <div>CommentBox</div>;
-};
+class CommentBox extends Component {
+	state = { comment: '' };
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		this.setState({ comment: '' });
+	};
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<h4>Add a Comment</h4>
+				<textarea
+					onChange={(e) => this.setState({ comment: e.target.value })}
+					value={this.state.comment}
+				/>
+				<div>
+					<button>Submit Comment</button>
+				</div>
+			</form>
+		);
+	}
+}
 
 export default CommentBox;
