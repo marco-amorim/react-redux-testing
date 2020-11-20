47821350c8560dc35bcac5524c3f17a4d6d7ea5b
marco-amorim <marcoamorim95@hotmail.com>
2020-11-20 20:20:15 -0300

    saveComment action creator

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,5 +1,10 @@
+import { SAVE_COMMENT } from 'actions/types';
+
 const commentsReducer = (state = [], action) => {
 	switch (action.type) {
+		case SAVE_COMMENT:
+			return [...state, action.payload];
+
 		default:
 			return state;
 	}
